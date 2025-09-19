c7e685bc3c61977600f8c6cfc6395ddb6be8d679
cauasturaro <caua.sturaro111@gmail.com>
2025-09-19 15:09:13 -0300

    feat: menu + bug fixes

--- carros.c
+++ carros.c
@@ -5,7 +5,7 @@
 struct Carro
 {
     int ano;
-    char chassi[8];
+    int chassi;
 };
 typedef struct Carro Carro;
 
@@ -19,32 +19,88 @@ Carro CriarCarro()
     return carro;
 }
 
-int main(void)
+void ListarCarros(Carro carros[], int quantidade)
 {
-    Carro carros[MAX_CARROS];
-
-    int continuar = 1;
-
-    for (int i = 0; i < MAX_CARROS; i++)
-    {
-        carros[i] = CriarCarro();
-
-        printf("Deseja continuar? (1 - Sim / 0 - Não): ");
-        scanf("%d", &continuar);
-
-        if (continuar == 0)
-        {
-            if (i < MAX_CARROS - 1)
-                carros[i + 1].ano = 0;
-            break;
-        }
-    }
-
     printf("------------------\n");
-    for (int i = 0; i < MAX_CARROS && carros[i].ano != "0"; i++)
+    for (int i = 0; i < quantidade; i++)
     {
         printf("Carro %d: Ano: %d, Chassi: %d\n", i + 1, carros[i].ano, carros[i].chassi);
     }
+    printf("------------------\n");
+}
+
+void criandoCarros(Carro carros[], int *qntCarros)
+{
+    int continuar = 1;
+    if (*qntCarros < MAX_CARROS)
+    {
+        for (int i = *qntCarros; i < MAX_CARROS; i++)
+        {
+            carros[i] = CriarCarro();
+            (*qntCarros)++;
+
+            printf("Deseja continuar? (1 - Sim / 0 - Não): ");
+            scanf("%d", &continuar);
+            if (continuar == 0)
+                break;
+        }
+    }
+    else
+    {
+        printf("Limite de carros atingido!\n");
+    }
+}
+
+int menu(Carro carros[], int *qntCarros)
+{
+    int opcao = 0;
+
+    do
+    {
+        printf("1. Adicionar Carro\n");
+        printf("2. Listar Carros\n");
+        printf("3. Sair\n");
+
+        scanf("%d", &opcao);
+
+        if (opcao < 1 || opcao > 3)
+        {
+            printf("Opção inválida! Tente novamente.\n");
+        }
+
+    } while (opcao < 1 || opcao > 3);
+
+    switch (opcao)
+    {
+    case 1:
+        criandoCarros(carros, qntCarros);
+        return 1;
+        break;
+    case 2:
+        ListarCarros(carros, *qntCarros);
+        return 1;
+        break;
+    case 3:
+        printf("Saindo...\n");
+        return 0;
+        break;
+    default:
+        printf("Opção inválida!\n");
+        return 1;
+        break;
+    }
+}
+
+int main(void)
+{
+    Carro carros[MAX_CARROS];
+    int qntCarros = 0;
+
+    int continuar;
+    do
+    {
+        continuar = menu(carros, &qntCarros);
+    } while (continuar != 0);
 
     return 0;
 }
